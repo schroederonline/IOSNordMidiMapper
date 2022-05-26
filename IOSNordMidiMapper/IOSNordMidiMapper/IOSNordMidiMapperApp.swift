cf536d8174a6cceb7192b388b0bd5872a2ed375a
Marcel Schröder <marcel@schroederonline.de>
2022-05-26 10:18:08 +0200

    leeres HelloWorld Projekt

---
+++ IOSNordMidiMapper/IOSNordMidiMapper/IOSNordMidiMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSNordMidiMapperApp.swift
+//  IOSNordMidiMapper
+//
+//  Created by Marcel Schröder on 10.04.21.
+//
+
+import SwiftUI
+
+@main
+struct IOSNordMidiMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSNordMidiMapper/IOSNordMidiMapperTests/IOSNordMidiMapperTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOSNordMidiMapperTests.swift
+//  IOSNordMidiMapperTests
+//
+//  Created by Marcel Schröder on 10.04.21.
+//
+
+import XCTest
+@testable import IOSNordMidiMapper
+
+class IOSNordMidiMapperTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
