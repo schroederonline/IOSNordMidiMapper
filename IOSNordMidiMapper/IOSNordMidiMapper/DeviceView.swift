511ede6ef5e0d570fdd0568de383519adbe8d474
Marcel Schröder <marcel@schroederonline.de>
2022-05-27 12:43:18 +0200

    DeviceView verbessert

--- IOSNordMidiMapper/IOSNordMidiMapper/DeviceView.swift
+++ IOSNordMidiMapper/IOSNordMidiMapper/DeviceView.swift
@@ -23,13 +23,50 @@ struct DeviceView: View {
                    Text(self.deviceName)
                        .font(.title)
 
+                   Text("Program Calculator").foregroundColor(Color.gray)
+                   Text("")
+                   Text("")
+                   HStack(){
+                       TextField("A:01", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/) .font(.title2)
+                       Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                           Text("Program Mode").tag(1)
+                           Text("Song Mode").tag(2)
+                           Text("Live Mode").tag(3)
+                       }.font(.title2)
+                   }
+                   Text("")
+                   Text("")
                    Divider()
-
-                   Text("Program Calculator")
-                       .font(.title2)
-                   Text("Descriptive text goes here.")
-               }
-               .padding()
+               }.padding()
+               
+               
+               VStack(alignment: .leading) {
+                   
+                   Text("Midi (1-128)" ).foregroundColor(Color.gray)
+                   Text("")
+                   Text("")
+                   
+                   HStack(){
+                       TextField("1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/) .font(.title2)
+                       Text("Bank").font(.title2)
+                   }
+                   Text("")
+                   Text("")
+                   
+                   HStack(){
+                       TextField("1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/) .font(.title2)
+                       Text("SubBank").font(.title2)
+                   }
+                   Text("")
+                   Text("")
+                   HStack(){
+                       TextField("1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/) .font(.title2)
+                       Text("Program").font(.title2)
+                   }
+                   
+               }.padding()
+             
+               
 
                Spacer()
            }
